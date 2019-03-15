92be137b6f8da236da034337ad954072a9f9760e
Devon Troedel <devon.troedel@gmail.com>
2019-03-14 21:23:31 -0500

    auto commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.show('Hello, welcome to my website!');
+	res.send('Hello, welcome to my website!');
 });
 
 const port = process.env.PORT || 3000;
