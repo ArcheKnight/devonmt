741157a7cd09d890e4b65e5e1487ece8f6b0f095
Devon Troedel <devon.troedel@gmail.com>
2019-03-15 20:20:23 +0000

    leaving work

--- routes/main.js
+++ routes/main.js
@@ -1,2 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
+const helpers = require('../middleware/helpers');
+const controllers = require('../controllers/main');
+
+router.get('/', helpers.getPageVars, (req, res) => {
+    controllers.landing(req, res)
+});
+
+module.exports = router;
