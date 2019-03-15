ef9fab52de26830ee0d2b716f882c5fb1c3e866b
Devon Troedel <devon.troedel@gmail.com>
2019-03-14 21:22:24 -0500

    commit

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+	res.show('Hello, welcome to my website!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
