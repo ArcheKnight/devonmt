cc5b1d651d0b6d713bd4b021e825262e8b16034e
Devon Troedel <devon.troedel@gmail.com>
2019-03-16 09:01:53 -0500

    minor update to topnav

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,29 @@ const router = require('express').Router();
 const helpers = require('../middleware/helpers');
 const controllers = require('../controllers/main');
 
+// GET landing page
 router.get('/', helpers.getPageVars, (req, res) => {
     controllers.landing(req, res)
 });
+
+// GET login page
+router.get('/login', helpers.getPageVars, (req, res) => {
+    controllers.getLogin(req, res);
+})
+
+// POST login page
+router.post('/login', (req, res) => {
+    controllers.postLogin(req, res);
+})
+
+// GET sign up page
+router.get('/login', helpers.getPageVars, (req, res) => {
+    controllers.getSignUp(req, res);
+})
+
+// POST sign up page
+router.post('/login', (req, res) => {
+    controllers.postSignUp(req, res);
+})
 
 module.exports = router;
