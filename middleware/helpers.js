2a425a159de2decbfb3cbbe4e4bcd27be32f4aec
Devon Troedel <devon.troedel@gmail.com>
2019-03-18 20:18:57 +0000

    leaving work

--- middleware/helpers.js
+++ middleware/helpers.js
@@ -3,7 +3,7 @@ const pathsJson = require('./paths.json');
 const helpers = {
     getPageVars: (req, res, next) => {
         const pageVars = pathsJson[req.path];
-
+        
         req.pageVars = pageVars;
         next();
     }
